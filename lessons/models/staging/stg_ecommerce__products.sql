a8e4d77ccace715ca3df3caf8ca718862693531f
Murali Mohan <mmohan.dataengineer@gmail.com>
2024-02-21 18:17:07 +0000

    commit on 21stFeb

---
+++ lessons/models/staging/stg_ecommerce__products.sql
@@ -0,0 +1,12 @@
+WITH source AS (
+	SELECT *
+	FROM {{ source('thelook_ecommerce', 'products') }}
+)
+SELECT
+	-- IDs
+	id AS product_id,
+	-- Other columns
+	cost,
+	retail_price,
+	department
+FROM source
