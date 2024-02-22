89101cbb303850d18ea9829bfc7ec3ee97b4aa5b
Murali Mohan <mmohan.dataengineer@gmail.com>
2024-02-22 20:28:16 +0000

    commit from 23rdFeb

--- lessons/models/staging/stg_ecommerce__products.sql
+++ lessons/models/staging/stg_ecommerce__products.sql
@@ -1,6 +1,7 @@
 WITH source AS (
 	SELECT *
-	FROM {{ source('thelook_ecommerce', 'products') }}
+	--FROM {{ source('thelook_ecommerce', 'products') }}
+	from {{ source('thelook_ecommerce','products') }}
 )
 SELECT
 	-- IDs
